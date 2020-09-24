46e204cd5a7a40e25a71f64dc9b753375e91a1e0
Takahiro.Hori <brightholidoy610@gmail.com>
2020-09-24 10:28:30 +0000

    Userモデルにテストデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+(1..5).each do |num|
+    user = User.new(name: 'test' + num.to_s , email: 'test' + num.to_s + '@gmail.com', password: 'test' + num.to_s)
+    user.save
+end
